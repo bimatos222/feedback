38c575f6ef3ac1521917df13c850712e8dd6453f
bianca 22A <bianca2018mm@gmail.com>
2024-04-30 00:08:21 +0000

    commit do JS completo

---
+++ pages/agradecimento.js
@@ -0,0 +1,8 @@
+function pegarnota() {
+    let nota  = localStorage.getItem("nota")
+
+    let paragrafonota = document.querySelector(".paragrafo_nota")
+    paragrafonota.innerHTML = `Você selecionou ${nota} de 5`
+}
+
+pegarnota()
